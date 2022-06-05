5167b713e224599b5c7e7f2c47f9df6d781ec73e
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-05 16:10:25 +0900

    apply grid layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Main = () => {
-  return <div>Main</div>;
+import MessageInputField from "./MessageInputField";
+import MessageList from "./MessageList";
+
+// チャット欄のスタイルの調整
+const useStyles = makeStyles({
+  root: {
+    display: "grid",
+    height: "100vh",
+    gridTemplateRows: "1fr auto",
+  },
+});
+
+const Main = ({ name }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <MessageList />
+      {/* nameを持たせるのはテキストを打ち込んで出力する際にアカウント名も表示する必要があるため */}
+      <MessageInputField name={name} />
+    </div>
+  );
 };
 
 export default Main;
