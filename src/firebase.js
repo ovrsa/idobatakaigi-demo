7dfb4ccea4322436863168874a960196bade3cca
ovrsa <92124533+ovrsa@users.noreply.github.com>
2022-06-05 22:11:36 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from "firebase";
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDWozcAwkKqwaGeVx7mrvCa4dgQWK3zTAg",
-  authDomain: "idobatakaigi-project.firebaseapp.com",
-  databaseURL: "https://idobatakaigi-project-default-rtdb.firebaseio.com/",
-  projectId: "idobatakaigi-project",
-  storageBucket: "idobatakaigi-project.appspot.com",
-  messagingSenderId: "559894479371",
-  appId: "1:559894479371:web:009eae82a87932c695fd0e",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
